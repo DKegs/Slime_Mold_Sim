7ac916012dae0b6dd8f2d0138918478475ff76f1
Daniel Keogler <98786287+DKegs@users.noreply.github.com>
2024-12-13 12:43:06 -0500

    Initial Slime_Sim test
    Getting initial conditions up and running.

---
+++ SlimeSim.py
@@ -0,0 +1,120 @@
+import pygame
+import random
+import math
+from collections import deque
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screenXsize = 600
+screenYsize = 600
+backgroundColor = (0, 0, 0)
+
+screen = pygame.display.set_mode((screenXsize, screenYsize))
+screen.fill(backgroundColor) 
+pygame.display.set_caption("Slime Sim")
+pygame.display.update()
+
+pi = math.pi
+
+agentCount = 50
+trailSize = 5
+trailEvaporationRate = 0.01
+#agentTrails = []
+
+
+class Agent:
+    def __init__(self, x: float, y: float, angle: float):
+        self.x = x # x position
+        self.y = y # y position
+        self.angle = angle       # (angle of movement)
+        self.trails = deque()
+        
+
+    def update(self):
+        # X wall bounce
+        if self.x < 0:
+            self.angle = random.uniform(pi * 1.5, pi * 2.5)
+        if self.x > screenYsize:
+            self.angle = random.uniform(pi * .5, pi * 1.5)
+        
+
+        # Y wall bounce
+        if self.y < 0:
+            self.angle = random.uniform(0, pi)
+        if self.y > screenXsize:
+            self.angle = random.uniform(pi, 2 * pi)
+
+        
+        # Forward movement
+        self.x += round(math.cos(self.angle), 1)
+        self.y += round(math.sin(self.angle), 1)
+        self.trails.append((self.x, self.y, 1.0))
+
+    def trailEvaporation(self):
+        
+        newTrails = []
+        surface = pygame.Surface((screenXsize, screenYsize), pygame.SRCALPHA)
+
+        for x, y, trailIntensity in self.trails:
+            newTrailIntensity = round(trailIntensity - trailEvaporationRate, 2)
+            if newTrailIntensity > 0:
+                newTrails.append((x, y, newTrailIntensity))
+                colorValue = int(newTrailIntensity * 255)
+                pygame.draw.rect(surface, (colorValue, colorValue, colorValue), (x, y, trailSize, trailSize))
+        
+        self.trails = newTrails
+        screen.blit(surface, (0, 0))
+        
+        # i = 0
+        # while i < (len(agentTrails)):
+        #     print(f'agentTrails = {len(agentTrails)}')
+            
+        #     agentTrails[i] = (agentTrails[i][0], agentTrails[i][1], round(agentTrails[i][2] - trailEvaporationRate, 2))
+        #     colorValue = agentTrails[i][2]
+        #     pygame.draw.rect(screen, (colorValue * 255, colorValue * 255, colorValue * 255), (agentTrails[i][0], agentTrails[i][1], trailSize, trailSize)) 
+        #     if agentTrails[i][2] == 0:
+        #         agentTrails.pop(i)
+        #     else:
+        #         i += 1
+
+    def drawAgent(self):
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, trailSize, trailSize))
+
+
+def main():
+
+    running = True
+
+    agents = []
+    for i in range(agentCount):
+        agent = Agent(
+            screenXsize / 2, # Spawns all agents in the middle of the screen
+            screenYsize / 2, # ^
+            (random.uniform(0, 2 * math.pi)) # Starts all agents looking in a random direction
+        )
+        agents.append(agent)
+    
+    while running:
+
+        for event in pygame.event.get(): # For loop through the event queue             
+            if event.type == pygame.QUIT: # Check for QUIT event
+                running = False
+
+        clock.tick(60) # 60 FPS
+        screen.fill(backgroundColor)
+
+
+        for agent in agents: 
+            agent.update()
+            agent.drawAgent()
+            agent.trailEvaporation()
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
+
+
+
+        
